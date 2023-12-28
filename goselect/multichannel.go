b5cdf052184bdb4defbaeb1eeaeccef36f86f048
ShenTong-2050 <1182887681@qq.com>
2023-12-28 12:57:08 +0800

    update multi channel select

--- goselect/multichannel.go
+++ goselect/multichannel.go
@@ -8,15 +8,13 @@ import (
 // select 多通道选择 应用
 
 func multiChannel(ch1 <-chan string,ch2 chan string,ch3 chan string) {
-	msg2 := "msg2"
+	msg2 := "hello world"
 	select {
 	case msg1 := <-ch1:
-		fmt.Printf("received msg from ch1 %v \n",msg1)
-	case ch2 <-msg2:
-	case msg3,ok := <-ch3 :
-		fmt.Printf("received msg from ch3 %v,ok:%v\n",msg3,ok)
-	default:
-		fmt.Printf("Unknown message\n")
+		fmt.Printf("read msg from chan1 %d\n",msg1)
+	case ch2<-msg2:
+	case msg3,ok := <-ch3:
+		fmt.Printf("read msg and chan is closed %d,%d\n",msg3,ok)
 	}
 }
 
@@ -24,29 +22,31 @@ func MultiChannelMain() {
 
 	ch1,ch2,ch3 := make(chan string),make(chan string),make(chan string)
 
+	// 往 ch1,ch3 生产消息
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			ch1<-"message one"
-			ch3<-"message three"
+			ch1 <-"message one"
+			ch3 <-"message three"
 		}
 	}()
 
-	go func() {
+	// 起个协程执行 消费 消息 操作
+	go func(){
 		for {
 			time.Sleep(time.Second)
 			multiChannel(ch1,ch2,ch3)
 		}
 	}()
 
-	// 读取 chan2 的 消息
+	// 接收 ch2 的 消息
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			fmt.Printf("read msg from chan2 %v \n",<-ch2)
+			fmt.Printf("read msg from chan2 %d \n",<-ch2)
 		}
 	}()
 
-	// 阻塞
+	// 阻塞主协程
 	<-(chan struct{})(nil)
 }
